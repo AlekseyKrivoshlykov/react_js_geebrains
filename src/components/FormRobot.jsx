ffcf0c2d06760e64eda3c7130590a3822d4e81a8
AlekseyKrivoshlykov <77828507+AlekseyKrivoshlykov@users.noreply.github.com>
2022-10-22 01:38:47 +1200

    Update FormRobot.jsx

--- src/components/FormRobot.jsx
+++ src/components/FormRobot.jsx
@@ -3,11 +3,13 @@ import React, { useEffect, useState } from 'react'
 
 export function FormRobot () {
     const [state, setState] = useState()
+    const [message, setMessage] = useState()
+
 
     const roboMess = [
         {
             Author: 'Robot',
-            Message: 'Your message is received.'
+            Message: 'Your message is received.',
         }
     ]
 
@@ -17,40 +19,39 @@ export function FormRobot () {
         event.preventDefault();
     }
 
-    // function handleChange (event) {
-    //     setState(event.target.value)
-    // }
- 
     useEffect(() => {
         setState(roboMess)
     }, [])
 
     function handleResponse (event) {
-         <div>
-            {roboMess.map(function(item) {
-                return <div key={item.id}>
-                <h3>{item.Author}</h3>
-                <p>{item.Message}</p>
-                </div>
-                })} 
-            </div>
-    }
-
+        setMessage(
+                <div>
+                    {roboMess.map(function(item, index) {
+               return (
+                   <div key={index}>
+                       <h3>{item.Author}</h3>
+                       <p>{item.Message}</p>
+                    </div>
+                    )
+               })} 
+           </div>
+        ) 
+   }
 
     return (
+        <>
             <div className="form_wrapp">
                 <form className="form_class" onSubmit={handleSubmit}>
-                    <input className="form_input" type="text" onChange={value => setState(values)} />
-                        {/* <input className="form_input" type="text" onChange={handleChange} /> */}
+                    <input className="form_input" type="text" onChange={(value) => setState(value)} />
                         <br/>
                         <button className="form_button" onClick={handleResponse}>Click</button>
-                        {/* <div>{roboMess}</div> */}
+                        <div>{message}</div>
+                        
                 </form>
-
-                
             </div>
-        
-       
-    )
+            
+
+            </>
+    )   
 
 }
